01bff1fa1de366d439c3c719e02bf6e6bffb6eb3
Jarkko Heinonen <71126476+JarkkoJake@users.noreply.github.com>
2021-12-15 15:57:12 +0200

    home router
    endpoints /, and /%topic% added and being used on home router

--- blogsite/main.js
+++ blogsite/main.js
@@ -1,14 +1,9 @@
 const express = require("express"),
 app = express(),
-router = express.Router(),
 layouts = require("express-ejs-layouts"),
 userRouter = require("./routes/userRouter"),
 blogRouter = require("./routes/blogRouter"),
-blog = require("./models/Blog"),
-user = require("./models/User"),
-blogController = require("./controllers/blogController"),
-userController = require("./controllers/userController"),
-homeController = require("./controllers/homeController"),
+homeRouter = require("./routes/homeRouter"),
 bodyParser = require("body-parser"),
 expressSession = require("express-session"),
 cookieParser = require("cookie-parser"),
@@ -19,26 +14,15 @@ app.use(bodyParser.json());
 app.use(layouts);
 app.use(express.static("public"));
 app.use("/user", userRouter);
-app.use("/", blogRouter);
+app.use("/", homeRouter);
+app.use("/blog", blogRouter);
+
+app.use((req, res, next) => {
+    redirectPath = res.locals.redirect;
+    if (redirectPath) res.redirect(redirectPath);
+    next();
+});
 
 app.set("view engine", "ejs");
-
-
-router.use(homeController.logRequestPaths);
-router.get("/", homeController.index);
-router.get("/music", homeController.music);
-router.get("/technology", homeController.technology);
-router.get("/nature", homeController.nature);
-router.get("/transport", homeController.transport);
-router.get("/other", homeController.other);
-router.use(express.static("public"));
-router.use(layouts);
-router.use(
-    express.urlencoded({
-        extended: false
-    })
-);
-app.use("/blog", router);
-
 
 app.listen(3000, () => {console.log("server running on port 3000")});
